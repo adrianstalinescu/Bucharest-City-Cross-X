4b98637d3861f6ca1e944d5d90f043b808493a4e
adrilo16 <36141252+adrilo16@users.noreply.github.com>
2020-03-07 00:44:38 +0200

    Added notification delete. Notifications > Capacity indicator switch. Started implementing Capacity indicator.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,56 +1,56 @@
 <template>
-  <div>
-    <div class="home-wrapper">
+  <div class="home-wrapper">
+    <v-card class="custom-notification-card" width="55vw" min-height="65px" outlined elevation="1">
+      <div class="custom-metro-card mt-2 metro-status-wrapper">
+        <v-chip class="ma-2 metro-status-icon" color="amber accent-4" text-color="black">
+          <span class="mr-2">M1</span>
+          <v-chip color="white" outlined class="metro-status-text">
+            <span>{{metroStatus1}}</span>
+          </v-chip>
+        </v-chip>
+        <v-chip class="ma-2 metro-status-icon" color="blue darken-4" text-color="black">
+          <span class="mr-2">M2</span>
+          <v-chip color="white" outlined class="metro-status-text">
+            <span>{{metroStatus2}}</span>
+          </v-chip>
+        </v-chip>
+        <v-chip class="ma-2 metro-status-icon" color="#ff2b1c" text-color="black">
+          <span class="mr-2">M3</span>
+          <v-chip color="white" outlined class="metro-status-text">
+            <span>{{metroStatus3}}</span>
+          </v-chip>
+        </v-chip>
+        <v-chip class="ma-2 metro-status-icon" color="green darken-3" text-color="black">
+          <span class="mr-2">M4</span>
+          <v-chip color="white" outlined class="metro-status-text">
+            <span>{{metroStatus4}}</span>
+          </v-chip>
+        </v-chip>
+      </div>
+      <h4 style="display:none">{{ metroStatus }}</h4>
+      <div class="custom-notification-capacity-switch mb-1">
+        <span class="custom-notification-switch">Notifications</span>
+        <v-switch inset v-model="switcher" class="custom-switcher"></v-switch>
+        <span class="custom-capacity-switch">Capacity Indicator</span>
+      </div>
+    </v-card>
+    <div v-if="switcher === false">
+      <h4 style="display:none">{{ notificationsKeys }}</h4>
+      <h4 style="display:none">{{ notifications }}</h4>
       <v-card
+        v-if="!notificationsKeys"
         class="custom-notification-card"
-        width="55vw"
-        min-height="65px"
+        width="25vw"
         outlined
         elevation="1"
       >
-        <div class="custom-metro-card mt-2 metro-status-wrapper">
-          <v-chip class="ma-2 metro-status-icon" color="amber accent-4" text-color="black">
-            <span class="mr-2">M1</span>
-            <v-chip color="white" outlined class="metro-status-text">
-              <span>{{metroStatus1}}</span>
-            </v-chip>
-          </v-chip>
-          <v-chip class="ma-2 metro-status-icon" color="blue darken-4" text-color="black">
-            <span class="mr-2">M2</span>
-            <v-chip color="white" outlined class="metro-status-text">
-              <span>{{metroStatus2}}</span>
-            </v-chip>
-          </v-chip>
-          <v-chip class="ma-2 metro-status-icon" color="#ff2b1c" text-color="black">
-            <span class="mr-2">M3</span>
-            <v-chip color="white" outlined class="metro-status-text">
-              <span>{{metroStatus3}}</span>
-            </v-chip>
-          </v-chip>
-          <v-chip class="ma-2 metro-status-icon" color="green darken-3" text-color="black">
-            <span class="mr-2">M4</span>
-            <v-chip color="white" outlined class="metro-status-text">
-              <span>{{metroStatus4}}</span>
-            </v-chip>
-          </v-chip>
-        </div>
-        <h4 style="display:none">{{ metroStatus }}</h4>
-      </v-card>
-      <h4 style="display:none">{{ notificationsKeys }}</h4>
-      <h4 style="display:none">{{ notifications }}</h4>
-      <v-card v-if="!notificationsKeys" class="custom-notification-card" width="25vw" outlined elevation="1">
         <div class="custom-notification-empty-card-wrap ma-2">
           <v-avatar elevation="0" color="light-blue lighten-3" class="mr-4 ml-2">
-            <v-icon color="light-blue darken-1" size="30">
-              mdi-exclamation-thick
-            </v-icon>
+            <v-icon color="light-blue darken-1" size="30">mdi-exclamation-thick</v-icon>
           </v-avatar>
-          <v-card-title class="custom-notification-empty-title" disabled>
-            There are no notifications
-          </v-card-title>
+          <v-card-title class="custom-notification-empty-title" disabled>There are no notifications</v-card-title>
         </div>
       </v-card>
-      <div>
       <v-card
         v-for="n in notificationsKeys"
         :key="n"
@@ -77,20 +77,60 @@
           >
             <v-icon color="light-blue darken-1" size="30">mdi-exclamation-thick</v-icon>
           </v-avatar>
-          <input :value="notifications[n].Title" disabled class="custom-notification-title ml-3 mr-3"/>
+          <input
+            :value="notifications[n].Title"
+            disabled
+            class="custom-notification-title ml-3 mr-3"
+          />
           <v-btn
             fab
             small
             elevation="0"
             color="transparent"
             class="custom-notification-button mr-4"
+            @click="deleteNotification(n)"
           >
             <v-icon color="red" size="30">mdi-close</v-icon>
           </v-btn>
         </div>
         <div class="custom-notification-card-wrap ma-3">{{notifications[n].Content}}</div>
       </v-card>
-      </div>
+      <v-snackbar v-model="snackbar" :timeout="2000" color="success">
+        <span class="notification-delete">The notification has been deleted</span>
+      </v-snackbar>
+    </div>
+    <div v-if="switcher === true">
+      <v-card class="custom-select-wrapper mt-2 pa-2">
+        <v-select v-model="subwaySelect" :items="stations" label="Select Station" hide-details color="green"
+          prepend-icon="mdi-subway">
+        </v-select>
+      </v-card>
+      <v-card class="custom-capacity-indicator-card">
+        <div class="custom-capacity-indicator-title-grid">
+          <v-chip color="#ffab00" class="capacity-indicator-line">M1<v-icon>mdi-chevron-right</v-icon>
+            <v-chip color="white" outlined><span class="capacity-indicator-direction">Republica</span></v-chip>
+          </v-chip>
+          <v-chip color="#3cbbd6" outlined class="capacity-indicator-arrival-wrapper"><span class="capacity-indicator-arrival">Arrival</span>
+            <v-chip color="#3cbbd6" outlined><span class="capacity-indicator-arrival-time">2 min</span></v-chip>
+          </v-chip>
+        </div>
+        <div class="custom-capacity-indicator-content-wrapper">
+          <div class="capacity">
+            <img class="carriage low"
+              src="http://shoreditch.opencapacity.co/components/dashboard-page/capacity-low.svg">
+            <img class="carriage medium"
+              src="http://shoreditch.opencapacity.co/components/dashboard-page/capacity-medium.svg">
+            <img class="carriage high"
+              src="http://shoreditch.opencapacity.co/components/dashboard-page/capacity-high.svg">
+            <img class="carriage low"
+              src="http://shoreditch.opencapacity.co/components/dashboard-page/capacity-low.svg">
+            <img class="carriage low"
+              src="http://shoreditch.opencapacity.co/components/dashboard-page/capacity-low.svg">
+            <img class="carriage low"
+              src="http://shoreditch.opencapacity.co/components/dashboard-page/capacity-low.svg">
+          </div>
+        </div>
+      </v-card>
     </div>
   </div>
 </template>
@@ -106,6 +146,12 @@ export default {
       metroStatus2: null,
       metroStatus3: null,
       metroStatus4: null,
+      snackbar: false,
+      switcher: false,
+      subwaySelect: '',
+      stations: [
+        'Timpuri Noi', 'Anghel Saligny',
+      ],
     };
   },
 
@@ -115,10 +161,10 @@ export default {
 
   computed: {
     notificationsKeys() {
-      return this.$store.getters.notificationsCount
+      return this.$store.getters.notificationsCount;
     },
     notifications() {
-      return this.$store.getters.notifications
+      return this.$store.getters.notifications;
     },
     metroStatus() {
       firebase
@@ -148,7 +194,20 @@ export default {
 
   mounted() {},
 
-  methods: {}
+  methods: {
+    deleteNotification(notification) {
+      firebase
+        .database()
+        .ref(
+          "Users/" +
+            this.$store.getters.user.uid +
+            "/Notifications/" +
+            notification
+        )
+        .remove();
+      this.snackbar = true;
+    }
+  }
 };
 </script>
 
@@ -201,6 +260,32 @@ export default {
   font-weight: 600;
 }
 
+.notification-delete {
+  margin-left: auto;
+  margin-right: auto;
+  font-size: 1rem;
+}
+
+.custom-notification-switch {
+  align-self: center;
+  justify-self: end;
+  font-weight: 500;
+}
+
+.custom-switcher {
+  margin-left: 17px;
+  justify-self: flex-end;
+  height: 35px;
+  margin-top: 0px;
+  align-self: center;
+}
+
+.custom-capacity-switch {
+  align-self: center;
+  justify-self: baseline;
+  font-weight: 500;
+}
+
 .metro-status-wrapper {
   font-weight: 800;
 }
@@ -222,4 +307,114 @@ export default {
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
 }
+
+.custom-notification-capacity-switch {
+  margin-left: auto;
+  margin-right: auto;
+  height: auto;
+  display: grid;
+  grid-template-columns: 5fr 1fr 5fr;
+}
+
+
+
+.custom-capacity-indicator-card {
+  width: 60vw;
+  height: auto;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 2vh;
+}
+
+.custom-capacity-indicator-title-grid {
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 2vh;
+  height: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
+
+.custom-capacity-indicator-content-wrapper {
+  display: block;
+  height: 5.4rem;
+  white-space: nowrap;
+  overflow: hidden;
+  margin: 0 1 rem;
+}
+
+.capacity-indicator-line {
+  font-weight: 800;
+  font-size: 1rem !important;
+  align-self: center;
+  justify-self: start;
+  margin-left: 0.8vw;
+  margin-top: 3vh;
+  margin-bottom: 3vh;
+  padding-right: 0px;
+}
+
+.capacity-indicator-direction {
+  font-weight: 700;
+  font-size: 1rem !important;
+  color: rgba(0,0,0,.87);
+}
+
+.capacity-indicator-arrival-wrapper {
+  font-weight: 800;
+  font-size: 1rem !important;
+  align-self: center;
+  justify-self: end;
+  margin-right: 0.8vw;
+  margin-top: 3vh;
+  margin-bottom: 3vh;
+  padding-right: 0px;
+}
+
+.capacity-indicator-arrival {
+  margin-right: 0.5rem;
+}
+
+.capacity-indicator-arrival-time {
+  font-weight: 800;
+  font-size: 1rem !important;
+}
+
+.capacity {
+  height: 80%;
+  background: url(http://shoreditch.opencapacity.co/components/dashboard-page/train.svg) -1px -1px no-repeat, #23282d;
+  background-size: auto calc(100% + 2px);
+  text-align: right;
+  animation: slide 2s 0s linear 1;
+  margin-left: 1rem;
+}
+
+.carriage {
+  display: inline;
+  width: 14%;
+  height: 2.9rem;
+  margin-top: 0.7rem;
+  margin-right: 0.7rem;
+  padding: 0.2rem 0;
+  vertical-align: bottom;
+}
+
+.carriage.low {
+  background: #82af5d;
+}
+
+.carriage.medium {
+  background: #f5bc28;
+}
+
+.carriage.high {
+  background: #df2f1f;
+}
+
+.custom-select-wrapper {
+  width: 42vw;
+  margin-left: auto;
+  margin-right: auto;
+}
+
 </style>
